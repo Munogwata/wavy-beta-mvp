478ce317a04e9fa406377224993d3f177f3ed3cc
Munozovepi Gwata <muno.gwata@gmail.com>
2022-01-17 01:35:45 +0200

    developing the navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { NavigationBarComponent } from './reusable-components/navigation-bar/navigation-bar.component';
 import { FooterComponent } from './reusable-components/footer/footer.component';
 import { ResultComponent } from './pages/result/result.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { NavBarComponent } from './reusable-components/nav-bar/nav-bar.component';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatListModule, MatNavList } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavigationBarComponent,
     FooterComponent,
-    ResultComponent
+    ResultComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
